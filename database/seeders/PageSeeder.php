ea6e632658d92d3399892861bacce2c4202e4148
rama-can <heyramacan@gmail.com>
2023-12-11 22:44:58 +0700

    Edit image for PageSeeder

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -19,7 +19,7 @@ class PageSeeder extends Seeder
                 'description' => 'Zakatel adalah sebuah lembaga nir laba yang mengelola zakat, infak, shodaqoh, dan wakaf dan mendistribusikannya kepada yang berhak sesuai syariat Islam.',
                 'content' => '<p>Zakatel adalah sebuah lembaga nir laba yang mengelola zakat, infak, shodaqoh, dan wakaf dan mendistribusikannya kepada yang berhak sesuai syariat Islam.</p>'
                     . '<p>Jumat, 1 Juli 2011 bertepatan dengan 29 Rajab 1432 H menjadi hari bersejarah bagi Gerakan Sosial Keagamaan di lingkungan Pensiun Telkom. Pada hari itu, PP P2Tel melalui beberapa anggotanya sepakat untuk mendirikan yayasan yaitu Lembaga Amil Zakat (LAZ) “Zakatel Citra Caraka” yang kemudian dikenal dengan nama “ZAKATEL”</p>',
-                'image' => 'images/general/logo_zakatel.png',
+                'image' => 'images/page.webp',
                 'status' => 'published',
             ],
         ];
