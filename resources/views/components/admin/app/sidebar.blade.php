d85f84b1b06a6098d15b95f797c67b1ab04a4e69
rama-can <heyramacan@gmail.com>
2023-12-11 23:07:00 +0700

    Fix sidebar active in menu platform

--- resources/views/components/admin/app/sidebar.blade.php
+++ resources/views/components/admin/app/sidebar.blade.php
@@ -146,7 +146,7 @@
                         <div class="lg:hidden lg:sidebar-expanded:block 2xl:block">
                             <ul class="pl-9 mt-1 @if(!in_array(Request::segment(2), ['users','settings'])){{ 'hidden' }}@endif" :class="open ? '!block' : 'hidden'">
                                 <li class="mb-1 last:mb-0">
-                                    <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('users.index')){{ '!text-indigo-500' }}@endif" href="{{ route('users.index') }}">
+                                    <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('users.index') || Route::is('users.edit')){{ '!text-indigo-500' }}@endif" href="{{ route('users.index') }}">
                                         <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Users</span>
                                     </a>
                                 </li>
